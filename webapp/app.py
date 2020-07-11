2e6a5fb1088b5076226a73bf36ddbb9e5f5e1668
ftnext <takuyafjp+develop@gmail.com>
2020-07-12 01:01:48 +0900

    [feat] brush web app up (use functions, specify max length)

--- webapp/app.py
+++ webapp/app.py
@@ -3,7 +3,42 @@ from PIL import Image
 from werkzeug.utils import secure_filename
 
 
+SHRINK_TARGET_EXTENSION = (".jpg", ".png")
+
 app = Flask(__name__, static_folder="images")
+
+
+def calculate_shrinked_size(width, height, max_length):
+    """Calculate shrinked size in proportion
+
+    The larger of width and height should be equal to max_length.
+
+    For exanple, width=500px, height=400px
+    width : height = max_length : new_height
+    i.e. new_height = max_length * height / width
+
+    if you swap the width and the height:
+    width : height = new_width : max_length
+    """
+    if width > height:
+        new_width = max_length
+        new_height = int(max_length * height / width)
+    else:
+        new_width = int(max_length * width / height)
+        new_height = max_length
+    return (new_width, new_height)
+
+
+def resize_image(image_path, save_path, max_length):
+    """Resize a given image so that the width and the height
+    is equal or less than max_length
+    """
+    image = Image.open(image_path)
+    width, height = image.size
+    if width > max_length or height > max_length:
+        shrinked_size = calculate_shrinked_size(width, height, max_length)
+        resized_image = image.resize(shrinked_size, Image.BICUBIC)
+        resized_image.save(save_path)
 
 
 @app.route("/resize", methods=["GET", "POST"])
@@ -12,11 +47,10 @@ def resize():
         return render_template("resize.html")
 
     image_file = request.files["image_file"]
+    max_length = int(request.form["max_length"])
     secure_name = secure_filename(image_file.filename)
-    image = Image.open(image_file)
-    resized_image = image.resize((300, 200))
     resized_image_path = f"images/{secure_name}"
-    resized_image.save(resized_image_path)
+    resize_image(image_file, resized_image_path, max_length)
     return render_template("resize.html", image_path=resized_image_path)
 
 
