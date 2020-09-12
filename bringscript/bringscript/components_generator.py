1cd317d2e369f3665df39e6193d747c04dc99486
ftnext <takuyafjp+develop@gmail.com>
2020-09-12 22:15:16 +0900

    [feat] add FlaskComponentsGenerator

--- bringscript/bringscript/components_generator.py
+++ bringscript/bringscript/components_generator.py
@@ -6,3 +6,10 @@ class EelComponentsGenerator(PreparableComponentsGenerator):
     def create(cls, generator_args, parent_dir, child_dir):
         renderer = TemplateRenderer.create()
         return cls(renderer, generator_args, parent_dir, child_dir)
+
+
+class FlaskComponentsGenerator(PreparableComponentsGenerator):
+    @classmethod
+    def create(cls, generator_args, parent_dir, child_dir):
+        renderer = TemplateRenderer.create()
+        return cls(renderer, generator_args, parent_dir, child_dir)
--- bringscript/tests/bringscript/test_components_generator.py
+++ bringscript/tests/bringscript/test_components_generator.py
@@ -39,3 +39,39 @@ class EelComponentsGeneratorTestCase(TestCase):
         self.assertEqual(actual._parent_dir, self.parent_dir)
         self.assertEqual(actual._child_dir, self.child_dir)
         renderer_create.assert_called_once_with()
+
+
+class FlaskComponentsGeneratorTestCase(TestCase):
+    def setUp(self):
+        self.renderer = MagicMock(spec=TemplateRenderer)
+        self.generator_args = MagicMock(spec=list)
+        self.parent_dir = MagicMock(spec=str)
+        self.child_dir = MagicMock(spec=str)
+
+    def test_init(self):
+        actual = components_generator.FlaskComponentsGenerator(
+            self.renderer, self.generator_args, self.parent_dir, self.child_dir
+        )
+
+        self.assertIsInstance(actual, PreparableComponentsGenerator)
+        self.assertEqual(actual._renderer, self.renderer)
+        self.assertEqual(actual._args, self.generator_args)
+        self.assertEqual(actual._parent_dir, self.parent_dir)
+        self.assertEqual(actual._child_dir, self.child_dir)
+
+    @patch("bringscript.core.TemplateRenderer.create")
+    def test_create(self, renderer_create):
+        actual = components_generator.FlaskComponentsGenerator.create(
+            self.generator_args,
+            parent_dir=self.parent_dir,
+            child_dir=self.child_dir,
+        )
+
+        self.assertIsInstance(
+            actual, components_generator.FlaskComponentsGenerator
+        )
+        self.assertEqual(actual._renderer, renderer_create.return_value)
+        self.assertEqual(actual._args, self.generator_args)
+        self.assertEqual(actual._parent_dir, self.parent_dir)
+        self.assertEqual(actual._child_dir, self.child_dir)
+        renderer_create.assert_called_once_with()
