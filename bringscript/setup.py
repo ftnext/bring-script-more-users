d0276220fdb7fa85da481fc398047987034f206f
ftnext <takuyafjp+develop@gmail.com>
2020-09-12 17:42:48 +0900

    [chore] add setup.py (configure to include jinja2 templates)
    * ref: https://setuptools.readthedocs.io/en/latest/setuptools.html#including-data-files

---
+++ bringscript/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="bringscript",
+    version="0.1.0",
+    packages=find_packages(exclude=["tests.*", "tests"]),
+    install_requires=["Jinja2"],
+    package_data={"bringscript": ["templates/*/*.jinja"]},
+)
