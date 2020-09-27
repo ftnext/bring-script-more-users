33e7f26bf6690e0bb51af92deb144b6cb8c5a992
ftnext <takuyafjp+develop@gmail.com>
2020-09-27 10:55:08 +0900

    [refactor] remove concrete args from entrypoint

--- bringscript/bringscript/main.py
+++ bringscript/bringscript/main.py
@@ -10,24 +10,12 @@ from bringscript.parser import parse_args
 
 def main():
     args = parse_args()
-    app_name = args.app_name
-    dest_dir = args.dest_dir
+    app_name: str = args.app_name
+    dest_dir: Path = args.dest_dir
 
     if args.mode == "gui":
-        generator_args = [
-            TemplateRenderArgument(
-                Path("gui/gui.py.jinja"),
-                Path(f"{dest_dir}/gui/gui.py"),
-                {"app_name": app_name},
-            ),
-            TemplateRenderArgument(
-                Path("gui/gui.html.jinja"),
-                Path(f"{dest_dir}/gui/web/{app_name}.html"),
-                {},
-            ),
-        ]
         components_generator = EelComponentsGenerator.create(
-            generator_args, parent_dir=f"{dest_dir}/gui", child_dir="web"
+            dest_dir, app_name, child_dir_name="web"
         )
         components_generator.generate()
 
