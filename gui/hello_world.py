d15ea56c88c865528a810b54474745f2ad565e49
ftnext <takuyafjp+develop@gmail.com>
2021-01-11 20:49:44 +0900

    [feat] try to expose Python class but it not works; it behaves function in JavaScript

--- gui/hello_world.py
+++ gui/hello_world.py
@@ -11,5 +11,12 @@ def say_hello():
     return f"Hello World {random.choice(NUMBERS)}"
 
 
+@eel.expose
+class Helloest:
+    @staticmethod
+    def say():
+        return "Hello World from Helloest class"
+
+
 eel.init("hello")
 eel.start("hello.html", size=(300, 200))
